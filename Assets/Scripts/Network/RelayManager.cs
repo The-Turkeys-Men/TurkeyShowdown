587221ab45c52dea4e890f84828c4c8117f98272
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2025-02-03 19:35:25 +0100

    Finishing relay system

--- Assets/Scripts/Network/RelayManager.cs
+++ Assets/Scripts/Network/RelayManager.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
+using Unity.Services.Matchmaker.Models;
+using Unity.Services.Qos;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -12,38 +17,26 @@ namespace Network
 {
     public class RelayManager : MonoBehaviour
     {
-        public async Task<string> CreateRelay(int maxPlayers)
+        private async void Start()
         {
             await UnityServices.InitializeAsync();
-            if (!AuthenticationService.Instance.IsSignedIn)
-            {
-                await AuthenticationService.Instance.SignInAnonymouslyAsync();
-            }
+            AuthenticationService.Instance.SignedIn += OnPlayerSignedIn;
+            AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
 
+        public async Task<string> CreateRelay(int maxPlayers)
+        {
+            
             try
             {
-                Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxPlayers);
+                (RelayServerData relayServerData, Allocation allocation) = await CreateHostAllocation(maxPlayers);
 
-                // Récupérer les données nécessaires pour configurer le transport
-                RelayServerData relayServerData = new RelayServerData(
-                    allocation.RelayServer.IpV4,
-                    (ushort)allocation.RelayServer.Port,
-                    allocation.AllocationIdBytes,
-                    allocation.ConnectionData,
-                    allocation.ConnectionData,
-                    allocation.Key,
-                    false, // Désactiver DTLS
-                    true   // Activer WebSockets (WSS)
-                );
+                NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
 
-                // Configurer Unity Transport
-                UnityTransport transport = FindObjectOfType<UnityTransport>();
-                transport.SetRelayServerData(relayServerData);
-
-                NetworkManager.Singleton.StartHost();
-
+                NetworkManager.Singleton.StartServer();
+                
                 string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-                Debug.Log("Join Code: " + joinCode);
+                Debug.Log("server started with join code: " + joinCode);
                 return joinCode;
             }
             catch (RelayServiceException e)
@@ -53,36 +46,43 @@ namespace Network
             }
         }
         
+        private async Task<(RelayServerData, Allocation)> CreateHostAllocation(int maxPlayer = 4)
+        {
+            var connectionType = "wss";
+            
+            Allocation hostAllocation = await RelayService.Instance.CreateAllocationAsync(maxPlayer);
+            RelayServerData newRelayData = hostAllocation.ToRelayServerData(connectionType);
+
+            return (newRelayData, hostAllocation);
+        }
+        
+        private async Task<RelayServerData> CreateJoinAllocation(string joinCode) 
+        {
+            string connectionType = "wss";
+            
+            var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+            RelayServerData newRelayData = joinAllocation.ToRelayServerData(connectionType);
+
+            return newRelayData;
+        }
+
+        private void OnPlayerSignedIn()
+        {
+            Debug.Log("signed in " + AuthenticationService.Instance.PlayerId);
+        }
 
         public async Task JoinRelay(string joinCode)
         {
             await UnityServices.InitializeAsync();
-            if (!AuthenticationService.Instance.IsSignedIn)
-            {
-                await AuthenticationService.Instance.SignInAnonymouslyAsync();
-            }
 
             try
             {
-                JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+                var relayServerData = await CreateJoinAllocation(joinCode);
 
-                // Récupérer les données nécessaires pour configurer le transport
-                RelayServerData relayServerData = new RelayServerData(
-                    joinAllocation.RelayServer.IpV4,
-                    (ushort)joinAllocation.RelayServer.Port,
-                    joinAllocation.AllocationIdBytes,
-                    joinAllocation.ConnectionData,
-                    joinAllocation.HostConnectionData,
-                    joinAllocation.Key,
-                    false, // Désactiver DTLS
-                    true   // Activer WebSockets (WSS)
-                );
-
-                // Configurer Unity Transport
-                UnityTransport transport = FindObjectOfType<UnityTransport>();
-                transport.SetRelayServerData(relayServerData);
+                NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
 
                 NetworkManager.Singleton.StartClient();
+                Debug.Log("client " + AuthenticationService.Instance.PlayerId + " joined relay");
             }
             catch (RelayServiceException e)
             {
