0e95bd2f400762d563080af83751224101ab451f
wighthood <zefires@gmail.com>
2025-02-11 10:52:57 +0100

    fixed respawn

--- Assets/Scripts/Respawn/RespawnButton.cs
+++ Assets/Scripts/Respawn/RespawnButton.cs
@@ -1,9 +1,10 @@
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class RespawnButton : MonoBehaviour
+public class RespawnButton : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private TextMeshProUGUI _RespawnButtonText;
     [SerializeField] private Button _RespawnButton;
@@ -18,14 +19,20 @@ public class RespawnButton : MonoBehaviour
         DeathScreen.SetActive(true);
     }
 
+ 
+    
     private void Update()
     {
-        time -= Time.deltaTime;
-        _RespawnButtonText.text = (int)time + " Sec\nRespawn";
-        if (time <= 0)
+        if (!_RespawnButton.interactable)
+        {
+            time -= Time.deltaTime;
+            _RespawnButtonText.text = (int)time + " Sec\nRespawn";
+        }
+        if (time <= 0 && !_RespawnButton.interactable)
         {
             _RespawnButtonText.text = "Respawn"; 
             _RespawnButton.interactable = true;
+            Debug.Log("Respawn");
         }
     }
     
@@ -34,5 +41,15 @@ public class RespawnButton : MonoBehaviour
         PlayerSpawner.SpawnerInstance.RespawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId);
        _RespawnButton.interactable = false;
        DeathScreen.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.pointerPressRaycast.gameObject == _RespawnButton.gameObject &&
+            eventData.button == PointerEventData.InputButton.Left && 
+            time <= 0)
+        { 
+            Respawn();
+        }
     }
 }
