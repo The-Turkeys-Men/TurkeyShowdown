22d02fe3927e6a9e6dd7b1e32f88e2b6d357c9cd
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2025-02-13 12:11:16 +0100

    Implementing skin system

--- Assets/Scripts/Customisation/ColorChanger.cs
+++ Assets/Scripts/Customisation/ColorChanger.cs
@@ -32,26 +32,6 @@ public class ColorChanger : NetworkBehaviour
         SpriteRenderer armFeatherSpriteRenderer = _spriteParentArmFeather.GetComponent<SpriteRenderer>();
 
         bodySpriteRenderer.sprite = _sprites[spriteName];
-    //     Debug.Log("Changing color to: " + color + " char count : " + color.Length);
-    //     Debug.Log("SpriteRenderers count: " + _spriteParent.childCount);
-    //     foreach (Transform spriteObject in _spriteParent)
-    //     {
-    //         SpriteRenderer spriteRenderer = spriteObject.GetComponent<SpriteRenderer>();
-    //         Debug.Log("Changing color of: " + spriteRenderer.name);
-    //         spriteRenderer.color = color.ToLower() switch
-    //         {
-    //             "red" => new Color32(255, 0, 0, 255 / 4),
-    //             "orange" => new Color32(255, 133, 27, 255 / 4),
-    //             "yellow" => new Color32(255, 220, 0, 255 / 4),
-    //             "green" => new Color32(46, 204, 64, 255 / 4),
-    //             "blue" => new Color32(0, 116, 217, 255 / 4),
-    //             "purple" => new Color32(177, 13, 201, 255 / 4),
-    //             "black" => new Color32(0, 0, 0, 255 / 4),
-    //             "white" => new Color32(255, 255, 255, 255 / 4),
-    //             "pink" => new Color32(255, 192, 203, 255 / 4),
-    //             _ => Color.clear
-    //         };
-    //     }
     }
     
     [Rpc(SendTo.ClientsAndHost)]
